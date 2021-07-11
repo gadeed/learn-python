9862ac364b4e61cc9139f38c0625ec8d403d792b
abdi <abdiahmed232@gmail.com>
2021-07-11 20:07:14 +0300

    waxan kudaray loops.py file

---
+++ basics/loops.py
@@ -0,0 +1,20 @@
+
+# Here is forloop
+cars = ['Nooho', 'Market', 'Qooqan', 'Caasi', 'Ina-taree']
+
+for car in cars:
+    if(car == 'Nooho'):
+        print(car.upper())
+    else:
+        print(car)
+
+
+# Here is While loop
+
+age = 5
+while age <= 15:
+    if(age == 15):
+        print("waxaad jirtaa " + str(age) + " waad qaan gaartay!")
+    else:
+        print("waxaad jirtaa " + str(age))
+    age += 1
