556f145bd915dab265daf393600ae23e30fcb35c
abdi <abdiahmed232@gmail.com>
2021-07-12 15:33:43 +0300

    add some code in variable.py

---
+++ basics/variables.py
@@ -0,0 +1,10 @@
+# here we define variables
+# calculating the number of hours,minutes
+
+unit = 'minutes'
+unit_calculation = 24*60
+
+print(f"30 days are {30 * unit_calculation} {unit}")
+print(f"60 days are {60 * unit_calculation} {unit}")
+print(f"100 days are {100 * unit_calculation} {unit}")
+print("100 days are " + str(100 * unit_calculation) + " " + str(unit))
